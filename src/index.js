ba3724d5a26abb6eec64512d8de805ac8ec8278d
Niels Koster <n.koster@portavita.eu>
2018-10-24 16:37:02 +0200

    Add gameLoop() and InputHandler()

--- src/index.js
+++ src/index.js
@@ -8,5 +8,20 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+new InputHandler(paddle);
 
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp;
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+
