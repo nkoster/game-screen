295bdf4ea6cbc4f54c6912b6b46b36cd4aa7170a
Niels Koster <n.koster@portavita.eu>
2018-10-24 23:17:16 +0200

    Initial, InputHandler() class.

---
+++ src/input.js
@@ -0,0 +1,16 @@
+export default class InputHandler {
+    constructor(paddle) {
+        document.addEventListener('keydown', (event) => {
+            switch(event.keyCode) {
+                case 37: paddle.moveLeft(); break;
+                case 39: paddle.moveRight(); break;
+            }
+        })
+        document.addEventListener('keyup', (event) => {
+            switch(event.keyCode) {
+                case 37: paddle.stop(); break;
+                case 39: paddle.stop(); break;
+            }
+        })
+    }
+}
