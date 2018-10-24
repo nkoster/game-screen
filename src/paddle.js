c8460bb1c7a530262fcd7af11adc63b6599176a7
Niels Koster <n.koster@portavita.eu>
2018-10-24 16:45:43 +0200

    my moves :)

--- src/paddle.js
+++ src/paddle.js
@@ -1,7 +1,11 @@
 export default class Paddle {
     constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
         this.width = 150;
         this.height = 20;
+        this.maxSpeed = 10;
+        this.speed = 0;
         this.position = {
             x: gameWidth / 2 - this.width / 2,
             y: gameHeight - this.height - 10
@@ -12,7 +16,18 @@ export default class Paddle {
         ctx.fillRect(this.position.x, this.position.y,
             this.width, this.height)
     }
-    update() {
-        this.position.x += 5;
+    moveLeft() {
+        if (this.position.x > 0) {
+            this.position.x -= 5
+        }
+    }
+    moveRight() {
+        if (this.position.x < this.gameWidth - this.width) {
+            this.position.x += 5
+        }
+    }
+    update(deltaTime) {
+        // if (!deltaTime) return;
+        // this.position.x += 5 / deltaTime;
     }
 }
