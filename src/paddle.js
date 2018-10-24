1b4c7aadeb6177003c4d30cecb11bd873996be7f
Niels Koster <n.koster@portavita.eu>
2018-10-24 22:03:08 +0200

    Better moving.

--- src/paddle.js
+++ src/paddle.js
@@ -4,7 +4,7 @@ export default class Paddle {
         this.gameHeight = gameHeight;
         this.width = 150;
         this.height = 20;
-        this.maxSpeed = 10;
+        this.maxSpeed = 7;
         this.speed = 0;
         this.position = {
             x: gameWidth / 2 - this.width / 2,
@@ -12,22 +12,21 @@ export default class Paddle {
         };
     }
     draw(ctx) {
-        ctx.fillStyle = '#0f0';
+        ctx.fillStyle = '#333';
         ctx.fillRect(this.position.x, this.position.y,
             this.width, this.height)
     }
     moveLeft() {
-        if (this.position.x > 0) {
-            this.position.x -= 5
-        }
+        this.speed = -this.maxSpeed;
     }
     moveRight() {
-        if (this.position.x < this.gameWidth - this.width) {
-            this.position.x += 5
-        }
+        this.speed = this.maxSpeed;
     }
     update(deltaTime) {
-        // if (!deltaTime) return;
-        // this.position.x += 5 / deltaTime;
+        if (!deltaTime) return;
+        this.position.x += this.speed;
+        if (this.position.x < 0) this.position.x = 0;
+        if (this.position.x > this.gameWidth - this.width)
+            this.position.x = this.gameWidth - this.width;
     }
 }
