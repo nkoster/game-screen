31f6b477b65fada61aec68f4ba9fb67ab4357f4d
Niels Koster <n.koster@portavita.eu>
2018-10-25 17:28:00 +0200

    Pass game size to Ball().

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-let ball = new Ball();
+let ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
 
 new InputHandler(paddle);
 
