12c2a04a51f2f2b233c1268cab27ca9d9fe11d67
Niels Koster <n.koster@portavita.eu>
2018-10-25 22:17:39 +0200

    Refactor.

--- src/game.js
+++ src/game.js
@@ -11,13 +11,12 @@ export default class Game {
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
         new InputHandler(this.paddle);
+        this.gameObjects = [ this.paddle, this.ball ];
     }
     update(deltaTime) {
-        this.paddle.update(deltaTime);
-        this.ball.update(deltaTime);
+        this.gameObjects.forEach(object => object.update(deltaTime));
     }
     draw(ctx) {
-        this.paddle.draw(ctx);
-        this.ball.draw(ctx);
+        this.gameObjects.forEach(object => object.draw(ctx));
     }
 }
