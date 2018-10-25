d4aa5ffdfbe3145bb9fc41e28691803898f40ba3
Niels Koster <n.koster@portavita.eu>
2018-10-25 22:58:33 +0200

    Initial, Brick() class.

---
+++ src/brick.js
@@ -0,0 +1,21 @@
+export default class Brick {
+    constructor(game, position) {
+        this.game = game;
+        this.image = document.getElementById('brick');
+        this.position = position;
+        this.width = 52;
+        this.height = 24;
+    }
+    update() {
+        //
+    }
+    draw(ctx) {
+        ctx.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+    }
+}
