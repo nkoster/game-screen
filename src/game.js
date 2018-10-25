7a350f2426c371c2ac91cec61b80c7cdcc859f16
Niels Koster <n.koster@portavita.eu>
2018-10-26 00:43:21 +0200

    Detect collisions.

--- src/game.js
+++ src/game.js
@@ -18,6 +18,7 @@ export default class Game {
     }
     update(deltaTime) {
         this.gameObjects.forEach(object => object.update(deltaTime));
+        this.gameObjects = this.gameObjects.filter(object => !object.markedForDeletion);
     }
     draw(ctx) {
         this.gameObjects.forEach(object => object.draw(ctx));
