f2bfb2dea3cd57332a9a6549d33ed40c973ae7ea
Niels Koster <n.koster@portavita.eu>
2018-10-25 15:39:22 +0200

    Add ball.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Paddle from './paddle.js';
 import InputHandler from './input.js';
+import Ball from './ball.js';
 
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
@@ -8,9 +9,9 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-new InputHandler(paddle);
+let ball = new Ball();
 
-paddle.draw(ctx);
+new InputHandler(paddle);
 
 let lastTime = 0;
 
@@ -20,8 +21,8 @@ function gameLoop(timestamp) {
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     paddle.update(deltaTime);
     paddle.draw(ctx);
+    ball.update(deltaTime);
+    ball.draw(ctx);
     requestAnimationFrame(gameLoop);
 }
-
-gameLoop();
-
+requestAnimationFrame(gameLoop);
