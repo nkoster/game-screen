26891cbb3a44451e9ed9aa626d7f07ebd46060cf
Niels Koster <n.koster@portavita.eu>
2018-10-25 23:00:29 +0200

    Test placing bricks.

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import Paddle from './paddle.js';
 import InputHandler from './input.js';
 import Ball from './ball.js';
+import Brick from './brick.js';
 
 export default class Game {
     constructor(gameWidth, gameHeight) {
@@ -10,8 +11,12 @@ export default class Game {
     start() {
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
+        this.bricks = [];
+        for (let i = 0; i < 10; i++) {
+            this.bricks.push(new Brick(this, { x: i*52, y: 30 }));
+        }
         new InputHandler(this.paddle);
-        this.gameObjects = [ this.paddle, this.ball ];
+        this.gameObjects = [ this.paddle, this.ball, ...this.bricks ];
     }
     update(deltaTime) {
         this.gameObjects.forEach(object => object.update(deltaTime));
